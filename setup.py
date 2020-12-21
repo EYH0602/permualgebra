aed90c81dc2deceafc107f653bee7bba8dd29c0a
Ethan He <yfhe@ucdavis.edu>
2020-12-20 17:57:40 -0800

    edit homepage link

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Calculate the permutations with Python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/EYH0602/permualgbra",
+    url="https://github.com/EYH0602/permualgebra",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
